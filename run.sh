ed97b17f9bd009c83401adf196fb8ff905864cd8
Nilay Patel <nilaypatel2@gmail.com>
2025-03-06 16:37:11 -0800

    Add run.sh and update the Dockerfile

---
+++ run.sh
@@ -0,0 +1,15 @@
+huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
+wandb login $(cat /etc/api-tokens/wandb-token)
+
+python src/formalize/align.py \
+    --model-name "unsloth/Meta-Llama-3.1-8B" \
+    --dataset "offendo/formal-align-redux" \
+    --output-dir "./formal_align" \
+    --max-tokens 2048 \
+    --seed 1234 \
+    --learning-rate "2e-6" \
+    --scheduler "constant" \
+    --optimizer "paged_adamw_8bit" \
+    --num-epochs 1 \
+    --batch-size 4 \
+    --gradient-accumulation 16
