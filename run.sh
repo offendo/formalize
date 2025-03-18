dc0846fd3a5c480c2e2ec4b7c3a860b4dd640ac1
Nilay Patel <nilaypatel2@gmail.com>
2025-03-18 10:29:48 -0700

    swap back to 8bit

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
 wandb login $(cat /etc/api-tokens/wandb-token)
 
+uv pip install torchao
+
 export WANDB_PROJECT='formal-align'
 export WANDB_RUN='with_eval_bs64ga4'
 
@@ -11,6 +13,6 @@ python src/formalize/align.py train \
     --output-dir "/volume/formal_align_$WANDB_RUN" \
     --max-tokens 2048 \
     --seed 1234 \
-    --learning-rate "2e-5" --scheduler "cosine" --optimizer "adamw_torch_4bit" \
+    --learning-rate "2e-5" --scheduler "cosine" --optimizer "paged_adamw_8bit" \
     --num-epochs 6 \
     --batch-size 64 --gradient-accumulation 4 --lora-rank 128
