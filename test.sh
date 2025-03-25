b565af77ab355b4d8ec427b01538534493c1a28d
Nilay Patel <nilaypatel2@gmail.com>
2025-03-25 10:34:37 -0400

    ready for test

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
 wandb login $(cat /etc/api-tokens/wandb-token)
 
 export WANDB_PROJECT='formal-align'
-export WANDB_RUN='v1'
 
 python src/formalize/align.py test \
     --model-name "meta-llama/Meta-Llama-3.1-8B" \
