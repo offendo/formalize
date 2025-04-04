e6416a945349e4bbc76d74ddd7629b9d739cd916
Nilay Patel <nilaypatel2@gmail.com>
2025-04-04 13:58:26 -0700

    only install lomo if needed

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
 wandb login $(cat /etc/api-tokens/wandb-token)
 
-pip install torchao lomo-optim
+pip install torchao
+if [[ $OPTIMIZER =~ .*lomo ]]; then
+  pip install lomo-optim
+fi;
 
 export WANDB_PROJECT='formal-align'
 
