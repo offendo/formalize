1df62de658fd0e5aee263b8616285ab7f4e80061
Nilay Patel <nilaypatel2@gmail.com>
2025-04-23 15:56:58 -0700

    add run_herald.sh

---
+++ run_herald.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip install -U vllm
+python run_herald.py \
+  --model FrenzyMath/Herald_translator \
+  --dataset offendo/math-atlas-titled-theorems \
+  --output_path /volume/math_atlas_herald_predictions.json
