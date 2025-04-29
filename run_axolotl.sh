d50b283c7dfabb87f217da6b2b64a91b2a71424c
Nilay Patel <nilaypatel2@gmail.com>
2025-04-29 14:59:17 -0700

    log in first

--- run_axolotl.sh
+++ run_axolotl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
+wandb login $(cat /etc/api-tokens/wandb-token)
+
 axolotl fetch deepspeed_configs
 axolotl train $CONFIG
