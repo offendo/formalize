df50b44c596339d77c406817c72fe9ecd2330f1e
Nilay Patel <nilaypatel2@gmail.com>
2025-05-03 15:02:05 -0700

    fix

--- run_em.sh
+++ run_em.sh
@@ -3,11 +3,8 @@
 huggingface-cli login --token $(cat /etc/api-tokens/hf-token)
 wandb login $(cat /etc/api-tokens/wandb-token)
 
-# Install xformers
-pip3 install xformers --index-url https://download.pytorch.org/whl/cu124
-
 axolotl fetch deepspeed_configs
 axolotl train $CONFIG
 
-# # merge the lora weights
-# axolotl merge-lora $CONFIG
+# merge the lora weights
+axolotl merge-lora $CONFIG
