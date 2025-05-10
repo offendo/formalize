a78bfe04e70978d7babb24f1b2f98fbb876fff56
Nilay Patel <nilaypatel2@gmail.com>
2025-05-10 16:46:46 -0700

    verifying works

--- run_verify.py
+++ run_verify.py
@@ -1,11 +1,29 @@
+import re
+import time
+import pandas as pd
+
 from argparse import ArgumentParser
 from lean_server.client import Lean4Client
-import pandas as pd
+
+def remove_informal_prefix(formal_statement: str) -> str:
+    block_pattern = r"/-.*? -/\n"
+    no_blocks = re.sub(block_pattern, "", formal_statement, flags=re.DOTALL)
+    inline_pattern = r"--.*?\n"
+    no_blocks_or_inline = re.sub(inline_pattern, "", no_blocks, flags=re.DOTALL)
+    return no_blocks_or_inline
+
+def anonymize(formal_statement: str):
+    # Make everything a theorem
+    formal_statement = formal_statement.replace('lemma', 'theorem', 1)
+    name_pattern = r"theorem\s*?( [\w_\d']+? )"
+    name = re.sub(name_pattern, "theorem anonymous ", formal_statement, flags=re.DOTALL)
+    return name
 
 if __name__ == "__main__":
     parser = ArgumentParser("verifier")
     parser.add_argument("--data", type=str, required=True)
     parser.add_argument("--output_json", type=str, required=True)
+    parser.add_argument("--num_samples", type=int, default=-1)
     args = parser.parse_args()
 
     # Boot up client
@@ -13,16 +31,21 @@ if __name__ == "__main__":
 
     # Read & format input data
     df = pd.read_json(args.data)
+    if args.num_samples > 0:
+        df = df[:args.num_samples]
+    df['formal_statement'] = df['formal_statement'].apply(remove_informal_prefix)
 
     records = [
-        {'proof': thm, 'custom_id': str(idx)} 
+        {'proof': anonymize(thm), 'custom_id': str(idx)} 
         for idx, thm in enumerate(df['formal_statement'])
     ]
 
     # Launch the query & wait for the response
     print('Querying server...will take some time')
+    tik = time.time()
     response = client.verify(records, timeout=30)
-    print('Done!')
+    tok = time.time()
+    print(f'Done in {tok-tik:0.2f}s!')
 
     # Parse the outputs
     results = []
@@ -33,7 +56,8 @@ if __name__ == "__main__":
         error = thm_output['error']
 
         # Extract compiler messages
-        msg = thm_output.get('response', {}).get('messages', [])
+        resp = thm_output.get('response', {})
+        msg = resp.get('messages', []) if resp else []
 
         # Check whether it's been verified or not
         success = True
--- run_verify.sh
+++ run_verify.sh
@@ -4,3 +4,4 @@ git clone https://github.com/project-numina/kimina-lean-server lean_server
 (cd lean_server && pip install -r requirements.txt)
 
 python run_verify.py --data $VERIFY_INPUT --output $VERIFY_OUTPUT
+echo "saved verified outputs at $VERIFY_OUTPUT"
