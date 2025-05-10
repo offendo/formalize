ed00be599cb02da47b8eeb6f8c94515aa434076d
Nilay Patel <nilaypatel2@gmail.com>
2025-05-10 16:55:40 -0700

    clone custom server

--- run_verify.sh
+++ run_verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/project-numina/kimina-lean-server lean_server
+git clone https://github.com/offendo/kimina-lean-server lean_server
 (cd lean_server && pip install -r requirements.txt)
 
 python run_verify.py --data $VERIFY_INPUT --output $VERIFY_OUTPUT
