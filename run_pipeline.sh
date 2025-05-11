89ba50d85b992e86fef3a51196c2be6536f354ba
Nilay Patel <nilaypatel2@gmail.com>
2025-05-10 17:01:23 -0700

    fix bash expression

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ $SKIP_TRAIN == "" ]; then
+if [ "$SKIP_TRAIN" = "" ]; then
   bash run_em.sh
 fi
-if [ $SKIP_INFERENCE == "" ]; then
+if [ "$SKIP_INFERENCE" = "" ]; then
   bash run_herald.sh
 fi
-if [ $SKIP_SCORE == "" ]; then
+if [ "$SKIP_SCORE" = "" ]; then
   bash run_align.sh
 fi
-if [ $SKIP_VERIFY == "" ]; then
+if [ "$SKIP_VERIFY" = "" ]; then
   bash run_verify.sh
 fi
